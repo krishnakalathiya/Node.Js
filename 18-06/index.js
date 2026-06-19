1f2f5af918d965f74f20c8dc8d7a4bb3017d1060
krishnakalathiya <krishnakalathiya03@gmail.com>
2026-06-19 11:00:07 +0530

    18-06

---
+++ 18-06/index.js
@@ -0,0 +1,16 @@
+// import express from 'express' // modulejs
+ const express = require('express') // commonjs
+const app = express()
+
+app.get("/" , (req , res) => {
+  res.send("Welcome to express.js server")
+})
+
+app.get("/home" , (req , res) => {
+  res.send("Welcome to Home Page")
+})
+
+
+app.listen(3001 , () => {
+  console.log("server start on port 3001");
+})
