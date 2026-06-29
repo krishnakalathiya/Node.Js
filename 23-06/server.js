fc1295da7d9da58eee2d38feb63ccbb3d16f3e24
krishnakalathiya <krishnakalathiya03@gmail.com>
2026-06-29 13:56:01 +0530

    29-06

--- 23-06/server.js
+++ 23-06/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./src/config/db.js";
+import Student from "./src/models/studentModel.js";
 
 import dns from "dns";
 dns.setServers(['8.8.8.8', '8.8.4.4']);
@@ -14,6 +15,17 @@ connectDB();
 app.get("/", (req, res) => {
     res.send("Server Running...");
 });
+
+app.get('/add-student' , async(req , res) => {
+    const student = await Student.create()  
+    res.send(student)
+})
+
+app.get('/students' , async(req , res) => {
+    const students = await Student.find();
+
+    res.send(students)
+})
 
 app.listen(port, () => {
     console.log(`Server Started on Port ${port}`);
